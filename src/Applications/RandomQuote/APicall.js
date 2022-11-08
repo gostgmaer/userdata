0b888ce0bd4c1d41ff1e6ef40d423073a36592e6
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-11-08 16:53:07 +0530

    Random Quetes

---
+++ src/Applications/RandomQuote/APicall.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { notifyerror, notifyinfo } from "Utils/Notification/notify";
+
+const APicall = ({ options, url, value, setValue }) => {
+  useEffect(() => {
+    const Loadquote = async () => {
+      try {
+        let res = await axios.get(url, options);
+        setValue(res.data);
+        notifyinfo("Data recived Success", 2000);
+      } catch (error) {
+        notifyerror(error.message, 2000);
+      }
+    };
+    Loadquote();
+  }, [options, url]);
+};
+export const GetQuotes = ({ options, url, value, setValue }) => {
+  const getAPIQuotes = async () => {
+    try {
+      let res = await axios.get(url, options);
+      setValue(res.data);
+      notifyinfo("Data recived Success", 2000);
+    } catch (error) {
+      notifyerror(error.message, 2000);
+    }
+  };
+  return (
+    <button
+      type="button"
+      onClick={getAPIQuotes}
+      class="btn  col-8 btn-outline-success"
+    >
+      Get Quotes
+    </button>
+  );
+};
+
+export default APicall;
---
+++ src/Applications/RandomQuote/Quotes.js
@@ -0,0 +1,30 @@
+import SimpleDate from "Components/ComponentBlock/SimpleDate";
+import SimpleHeading from "Components/ComponentBlock/SimpleHeading";
+import React,{useState} from "react";
+import APicall from "./APicall";
+import Show from "./Show";
+
+const Quotes = () => {
+  const title = "Random Quote Generation";
+  const url = "https://quotes15.p.rapidapi.com/quotes/random/";
+
+  const options = {
+    headers: {
+      "X-RapidAPI-Key": "8842f2a974msh6b0949e628a5e32p13683ejsn3912b12003ed",
+      "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+    },
+    param: {},
+  };
+
+  const [value, setValue] = useState({});
+  return (
+    <React.Fragment>
+      <SimpleHeading title={title}></SimpleHeading>
+      <APicall value={value} setValue={setValue} options={options} url={url}></APicall>
+      <Show value={value} setValue={setValue} options={options} url={url} ></Show>
+      <SimpleDate></SimpleDate>
+    </React.Fragment>
+  );
+};
+
+export default Quotes;
