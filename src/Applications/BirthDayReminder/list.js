bbbcf9526293872b947efe5c69a6bb3d6a0e9d49
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-11-10 11:14:09 +0530

    List Data

--- src/Applications/BirthDayReminder/list.js
+++ src/Applications/BirthDayReminder/list.js
@@ -1,32 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Data from "./Data";
+import moment from "moment/moment";
 
 const List = () => {
-  const today = new Date().formatUTC()
-  console.log(today.toLocaleString());
-  // Data.forEach(element => {
-  //    console.log(element.dbo.toLocaleString());
-    
+  const today = new Date();
+  // Data.forEach((element) => {
+  //   let date = moment(element.dbo).format("MM/DD");
+  //   console.log(date);
   // });
-  // let arrray = Data.filter((dbo)=>dbo.dbo.getUTCMonth()===today.getUTCMonth())
-  // console.log(arrray);
+  let arrray;
+  useEffect(() => {
+   
+  }, []);
+  
+  arrray = Data.filter(
+    (element) =>
+      moment(element.dbo).format("MM/DD") === moment(today).format("MM/DD")
+  );
+  const clearAllHandle = () => {
+    arrray.length = 0;
+    console.log(arrray);
+  };
   return (
     <div className=" m-auto col-5">
-       <div class="card border-primary ">
-      <div class="card-body">
-        <h4 class="card-title">{Data.length} Birthday Today</h4>
-        <div>
-          <img class="card-img-top" src="holder.js/100px180/" alt="" />
-          <p class="card-text">Text</p>
+      <div class="card border-primary ">
+        <div class="card-body">
+          <h4 class="card-title">{arrray.length} Birthday Today</h4>
+          <ul className=" list-group">
+            {arrray.map((data) => {
+              return (
+                <li className="list-group-item d-flex" key={data.id}>
+                  <img
+                    class=""
+                    width={50}
+                    height={50}
+                    src={data.image}
+                    alt={data.first_name}
+                  />
+                  <div>
+                    <p class="card-text ms-2">
+                      {data.first_name} {data.last_name}
+                    </p>
+                    <p class="card-text ms-2">
+                      {moment(today).format("YYYY") -
+                        moment(data.dbo).format("YYYY")}{" "}
+                      Years Old
+                    </p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className=" card-footer">
+          <button
+            type="button"
+            class="btn btn-primary"
+            onClick={clearAllHandle}
+          >
+            Clear All
+          </button>
         </div>
       </div>
-      <div className=" card-footer">
-        <button type="button" class="btn btn-primary">Clear All</button>
-      </div>
     </div>
-
-    </div>
-   
   );
 };
 
